dd7ad3bfc4280e2885ecb18f2557e16df61b9a75
uWuWarriors <130533851+uWuWarriors@users.noreply.github.com>
2023-12-21 22:03:53 +0600

    Add files via upload

---
+++ CLOWN/CLOWN/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CLOWN
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //nameBD nameBD = new nameBD();
+            //ProductsDG.ItemsSource = nameBD.nameTable.ToList();
+            //CategoryDG.ItemsSource = tourspr420.nameTable.ToList();
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window_Edit win2 = new Window_Edit();
+            win2.Show();
+            //ProductsDG.ItemsSource = nameBD.nameTable.ToList();
+            //CategoryDG.ItemsSource = nameBD.nameTable.ToList();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            //var nameSelectP = ProductsDG.SelectedItems.Cast<nameTable>().ToList();
+            //var nameSelectC = CategoryDG.SelectedItems.Cast<nameTable>().ToList();
+            //NameBD.GetContext().Products.RemoveRange(nameSelectP);
+            //NameBD.GetContext().Category.RemoveRange(nameSelectC);
+            //NameBD.GetContext.SaveChanges();
+        }
+    }
+}
---
+++ CLOWN/CLOWN/Window_Edit.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CLOWN
+{
+    /// <summary>
+    /// Логика взаимодействия для Window_Edit.xaml
+    /// </summary>
+    public partial class Window_Edit : Window
+    {
+        public Window_Edit()
+        {
+            InitializeComponent();
+            //nameBD nameBD = new nameBD();
+        }
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win1 = new MainWindow();
+            win1.Show();
+
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var my = Test.Text;
+        }
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            //TourBaseEntities.GetContext().Hotels.Add(my);
+        }
+    }
+}
